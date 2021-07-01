1bb91b59eab41ea9f208fee8c411791e1aa010bd
nosf <tomefelipe0@usp.br>
2021-07-01 00:03:07 -0300

    <final development of tree building>

--- new-vers/new-vers-base.py
+++ new-vers/new-vers-base.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Node:
 
-    def __init__(self, center, mass):
+    def __init__(self):
         self.node1 = None
         self.node2 = None
         self.node3 = None
@@ -11,59 +11,50 @@ class Node:
         self.node6 = None
         self.node7 = None
         self.node8 = None
-        self.mass = mass
-        self.center = center
+        self.mass = 0
+        self.center = 0
     
-    def insert(self, center, mass, bounds):
+    def insert_and_build(self, pos, mass, bounds, sis):
+        flag = np.argwhere(np.all(sis > bounds[0], axis=1) & np.all(sis < bounds[1], axis=1)).shape[0]
+        self.mass += mass
+        self.center += mass*pos
+        if flag == 1:
+            return
         aux = np.concatenate(([bounds[0]], [bounds[1]/2]), axis=0) 
         aux2 = np.concatenate(([bounds[1]/2], [bounds[1]]), axis=0) 
-        if center[0] > bounds[1][0]/2:
-            if center[1] > bounds[1][1]/2:
-                if center[2] > bounds[1][2]/2:
-                    if self.node1 == None:
-                        self.node1 = Node(center, mass)
-                    else:
-                        self.node1.insert(center, mass, aux2)
+        if pos[0] > bounds[1,0]/2:
+            if pos[1] > bounds[1,1]/2:
+                if pos[2] > bounds[1,2]/2:
+                        self.node1 = Node()
+                        self.node1.insert(pos, mass, aux2, sis)
                 else:
-                     if self.node2 == None:
-                        self.node2 = Node(center, mass)
-                    else:
-                        self.node2.insert(center, mass, np.concatenate((aux2.T[:][ :-1], aux.T[:][-1:])).T
+                        self.node2 = Node()
+                        self.node2.insert(pos, mass, np.concatenate((aux2[:,:-1], aux[:,-1:]), axis=1), sis)
                    
-            elif center[2] > bounds[1][2]/2:
-                    if self.node3 == None:
-                        self.node3 = Node(center, mass)
-                    else:
-                        self.node3.insert(center, mass, np.concatenate())
+            elif pos[2] > bounds[1,2]/2:
+                        self.node3 = Node()
+                        self.node3.insert(pos, mass, np.concatenate((aux2[:,:-2], aux[:,1:-1], aux2[:,-1:]), axis=1), sis)
             else:
-                    if self.node4 == None:
-                        self.node4 = Node(center)
-                    else:
-                        self.node4.insert(center, mass, np.concatenate())
-        elif center[1] > bounds[1][1]/2:
-            if center[2] > bounds[1][2]:
-                    if self.node5 == None:
-                        self.node5 = Node(center, mass)
-                    else:
-                        self.node5.insert(center, mass, np.concatenate())
-            else:
-                    if self.node6 == None:
-                        self.node6 = Node(center, mass)
-                    else:
-                        self.node6.insert(center, mass, np.concatenate())
-        elif center[2] > bounds[1][2]/2:
-                    if self.node7 == None:
-                        self.node7 = Node(center, mass)
-                    else:
-                        self.node7.insert(center, mass, np.concatenate())
+                        self.node4 = Node()
+                        self.node4.insert(pos, mass, np.concatenate((aux2[:,:-2], aux[:,-2:]), axis=1), sis)
+        elif pos[1] > bounds[1,1]/2:
+            if pos[2] > bounds[1,2]:
+                        self.node5 = Node()
+                        self.node5.insert(pos, mass, np.concatenate((aux[:,:-2], aux2[:,-2:]), axis=1), sis)
+            else:               
+                        self.node6 = Node()
+                        self.node6.insert(pos, mass, np.concatenate((aux[:,:-2], aux2[:,1:-1], aux[:,-1:]), axis=1), sis)
+        elif pos[2] > bounds[1,2]/2:
+                        self.node7 = Node()
+                        self.node7.insert(pos, mass, np.concatenate((aux[:,:-1], aux2[:,-1:]), axis=1), sis)
         else:
-                    if self.node8 == None:
-                        self.node8 = Node(center, mass)
-                    else:
-                        self.node8.insert(center, mass, np.concatenate())
+                        self.node8 = Node()
+                        self.node8.insert(pos, mass, aux, sis)
+
+
 n = 1000
 nNonBar = 900
-bounds = np.array()
+bounds = np.array([1])
 sis = ((7/4)**1/3)*np.random.rand(n, 3)
 v = np.random.rand(n, 3)
 barMass = 1
