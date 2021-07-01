efb71eccf8461e424c63a1eb4a5437e6c0d1b0be
nosf <tomefelipe0@usp.br>
2021-07-01 14:49:21 -0300

    <tree insertion finished>

--- new-vers/new-vers-base.py
+++ new-vers/new-vers-base.py
@@ -14,52 +14,112 @@ class Node:
         self.mass = 0
         self.center = 0
     
-    def insert_and_build(self, pos, mass, bounds, sis):
+    def insert(self, pos, mass, bounds, sis):
+        
         flag = np.argwhere(np.all(sis > bounds[0], axis=1) & np.all(sis < bounds[1], axis=1)).shape[0]
+        if flag == 0:
+            return
+        
         self.mass += mass
         self.center += mass*pos
+
         if flag == 1:
             return
-        aux = np.concatenate(([bounds[0]], [bounds[1]/2]), axis=0) 
-        aux2 = np.concatenate(([bounds[1]/2], [bounds[1]]), axis=0) 
-        if pos[0] > bounds[1,0]/2:
-            if pos[1] > bounds[1,1]/2:
-                if pos[2] > bounds[1,2]/2:
-                        self.node1 = Node()
-                        self.node1.insert(pos, mass, aux2, sis)
+
+        middle = (bounds[1] - bounds[0]) / 2
+        aux = np.concatenate(([bounds[0]], [bounds[0] + middle]), axis=0) 
+        aux2 = np.concatenate(([bounds[0] + middle], [bounds[1]]), axis=0)
+
+        if pos[0] > aux[1,0]:
+            if pos[1] > aux[1,1]:
+                if pos[2] > aux[1,2]:
+
+                    self.node1 = Node()
+                    self.node1.insert(pos, mass, aux2, sis)
+                    return
+                
                 else:
-                        self.node2 = Node()
-                        self.node2.insert(pos, mass, np.concatenate((aux2[:,:-1], aux[:,-1:]), axis=1), sis)
+
+                    self.node2 = Node()
+                    self.node2.insert(pos, mass, np.concatenate((aux2[:,:-1], aux[:,-1:]), axis=1), sis)
+                    return
                    
-            elif pos[2] > bounds[1,2]/2:
-                        self.node3 = Node()
-                        self.node3.insert(pos, mass, np.concatenate((aux2[:,:-2], aux[:,1:-1], aux2[:,-1:]), axis=1), sis)
+            elif pos[2] > aux[1,2]:
+
+                self.node3 = Node()
+                self.node3.insert(pos, mass, np.concatenate((aux2[:,:-2], aux[:,1:-1], aux2[:,-1:]), axis=1), sis)
+                return
+            
             else:
-                        self.node4 = Node()
-                        self.node4.insert(pos, mass, np.concatenate((aux2[:,:-2], aux[:,-2:]), axis=1), sis)
-        elif pos[1] > bounds[1,1]/2:
-            if pos[2] > bounds[1,2]:
-                        self.node5 = Node()
-                        self.node5.insert(pos, mass, np.concatenate((aux[:,:-2], aux2[:,-2:]), axis=1), sis)
-            else:               
-                        self.node6 = Node()
-                        self.node6.insert(pos, mass, np.concatenate((aux[:,:-2], aux2[:,1:-1], aux[:,-1:]), axis=1), sis)
-        elif pos[2] > bounds[1,2]/2:
-                        self.node7 = Node()
-                        self.node7.insert(pos, mass, np.concatenate((aux[:,:-1], aux2[:,-1:]), axis=1), sis)
+
+                self.node4 = Node()
+                self.node4.insert(pos, mass, np.concatenate((aux2[:,:-2], aux[:,-2:]), axis=1), sis)
+                return
+        
+        elif pos[1] > aux[1,1]:
+            if pos[2] > aux[1,2]:
+           
+                self.node5 = Node()
+                self.node5.insert(pos, mass, np.concatenate((aux[:,:-2], aux2[:,-2:]), axis=1), sis)
+                return
+            
+            else:
+             
+                self.node6 = Node()
+                self.node6.insert(pos, mass, np.concatenate((aux[:,:-2], aux2[:,1:-1], aux[:,-1:]), axis=1), sis)
+                return
+        
+        elif pos[2] > aux[1,2]:
+        
+            self.node7 = Node()
+            self.node7.insert(pos, mass, np.concatenate((aux[:,:-1], aux2[:,-1:]), axis=1), sis)
+            return
+                    
         else:
-                        self.node8 = Node()
-                        self.node8.insert(pos, mass, aux, sis)
+      
+            self.node8 = Node()
+            self.node8.insert(pos, mass, aux, sis)
+            return
 
+    def build_tree(self, sis, masses, bounds):
+        for pos, mass in zip(sis, masses):
+            self.insert(pos, mass, bounds, sis)
 
-n = 1000
-nNonBar = 900
-bounds = np.array([1])
-sis = ((7/4)**1/3)*np.random.rand(n, 3)
+    def calc_forces(self):
+        if self == None: 
+            return
+
+        aux = np.concatenate(([bounds[0]], [bounds[1]/2]), axis=0) 
+        aux2 = np.concatenate(([bounds[1] / 2], [bounds[1]]), axis=0) 
+        dist = np.sqrt(np.sum((pos - self.center / self.mass)**2))
+        leng = abs(bounds[0][0] - bounds[1][0])
+        
+        if leng / dist < 1:
+            accel += (self.mass * (pos - center)) / (dist**3 )
+        
+        else:
+            self.node1.calc_forces(pos, mass, aux2, sis)
+            self.node2.calc_forces(pos, mass, np.concatenate((aux2[:,:-1], aux[:,-1:]), axis=1), sis)             
+            self.node3.calc_forces(pos, mass, np.concatenate((aux2[:,:-2], aux[:,1:-1], aux2[:,-1:]), axis=1), sis)
+            self.node4.calc_forces(pos, mass, np.concatenate((aux2[:,:-2], aux[:,-2:]), axis=1), sis)
+            self.node5.calc_forces(pos, mass, np.concatenate((aux[:,:-2], aux2[:,-2:]), axis=1), sis)
+            self.node6.calc_forces(pos, mass, np.concatenate((aux[:,:-2], aux2[:,1:-1], aux[:,-1:]), axis=1), sis)
+            self.node7.calc_forces(pos, mass, np.concatenate((aux[:,:-1], aux2[:,-1:]), axis=1), sis)
+            self.node8.calc_forces(pos, mass, aux, sis)
+
+def calc_bounds(sis):
+    return np.array([np.array(3*[np.amin(sis)-10]), np.array(3*[np.amax(sis)+10])])
+
+root = Node()
+n = 10
+nNonBar = 9
+sis = np.random.rand(n, 3)
+bounds = calc_bounds(sis)
 v = np.random.rand(n, 3)
 barMass = 1
 nonBarMass = 1
-k = 1
-G = 1
 masses = np.concatenate((nNonBar*[nonBarMass], (n-nNonBar)*[barMass]), axis=None)
+root.build_tree(sis, masses, bounds)
+print(root.mass)
+print(root.center/root.mass)
 
